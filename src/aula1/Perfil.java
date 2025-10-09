6b07b14fa447b3006c9b07ae9c03a6de43c2cfb9
iamandaleao <amandabibiano144@gmail.com>
2025-10-09 17:33:52 -0300

    feat: add profile class

---
+++ src/aula1/Perfil.java
@@ -0,0 +1,24 @@
+package aula1;
+
+/*Crie um projeto Java no IntelliJ que deve conter uma classe chamada Perfil, com o main, que ao executar deverá imprimir um cumprimento personalizado no console. Por exemplo, "Olá, [Seu Nome]!".*/
+
+
+/*public class Perfil{
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Nome");
+
+    }
+
+}*/
+
+/*Modifique o programa para imprimir uma segunda mensagem de cumprimento personalizada. Por exemplo, "Tudo bem?".*/
+
+public class Perfil{
+
+    public static void main(String[] args) {
+        System.out.println("Olá, Nome");
+        System.out.println("Tudo bem?");
+    }
+
+}
