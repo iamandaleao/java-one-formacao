e41b32b90992af509dd9cb7a342fd972dba9cce7
iamandaleao <amandabibiano144@gmail.com>
2025-10-09 19:58:37 -0300

    feat: start Screen Match project

---
+++ src/aula1/screenmatch.java
@@ -0,0 +1,13 @@
+package aula1;
+
+public class screenmatch {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluindoNoPlano = true;
+        double notaDoFilme = 8.1;
+    }
+}
