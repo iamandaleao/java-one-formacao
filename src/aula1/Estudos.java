4d00d25f456b9f2f853813cff2ea3bd32bb8280a
iamandaleao <amandabibiano144@gmail.com>
2025-10-09 17:32:10 -0300

    feat: add study exercises

---
+++ src/aula1/Estudos.java
@@ -0,0 +1,12 @@
+package aula1;
+
+public class Estudos {
+   /* Crie uma classe chamada Estudos para imprimir anotações de estudos no console.*/
+
+
+       public static void main(String[] args) {
+           System.out.println("JVM (Java Virtual Machine) - é a Máquina Virtual do Java responsável por executar o bytecode");
+           System.out.println("JRE (Java Runtime Environment) - Ambiente de Execução do Java que fornece as bibliotecas padrões do Java para o JDK compilar o seu código e para a JVM executar o seu program");
+       }
+
+}
