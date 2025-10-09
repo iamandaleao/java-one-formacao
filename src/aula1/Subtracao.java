55a7c3d707854633275478c617cc42a28b60739a
iamandaleao <amandabibiano144@gmail.com>
2025-10-09 17:32:59 -0300

    feat: add subtraction program

---
+++ src/aula1/Subtracao.java
@@ -0,0 +1,9 @@
+package aula1;
+
+/*Crie uma classe Subtracao para imprimir o resultado de 10 - 5 no console.*/
+public class Subtracao{
+
+    public static void main(String[] args) {
+        System.out.println(10 - 5);
+    }
+}
