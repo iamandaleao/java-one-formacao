6bd0aadf54dc4778436b7a89eb2ea803d0854e17
iamandaleao <amandabibiano144@gmail.com>
2025-10-10 12:48:33 -0300

    feat: add relational operators examples

---
+++ src/operadores/OperadoresRelacionais.java
@@ -0,0 +1,28 @@
+package operadores;
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+
+
+int a = 10; // Atribui o valor 10 à variável a
+int b = 5; // Atribui o valor 5 à variável b
+int c = 30; // Atribui o valor 30 à variável c
+
+boolean igual = (b == a);
+//Nesse caso a variável igual ficará com o valor *false*, pois o valor de b não é igual o valor de a.
+
+boolean diferente = (b != c);
+//A variável diferente ficará com o valor *true*, pois o valor de b é diferente do valor de c.
+
+boolean maior = (b > a);
+//A variável maior ficará com o valor *false*, pois o valor de b é menor que o valor de a.
+
+boolean menorIgual = (b <= c);
+//A variável menorIgual ficará com o valor *true*, pois o valor de b é menor que o valor de c.
+
+// Exibindo os resultados no terminal
+        System.out.println("b é igual a a? " + igual);
+        System.out.println("b é diferente de c? " + diferente);
+        System.out.println("b é maior que a? " + maior);
+        System.out.println("b é menor ou igual a c? " + menorIgual);
+    }
+}
