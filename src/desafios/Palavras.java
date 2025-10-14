09615a31b72b180868671b3c1bf5fc509e3dd050
iamandaleao <amandabibiano144@gmail.com>
2025-10-13 21:38:19 -0300

    feat: word handling example in Palavras

---
+++ src/desafios/Palavras.java
@@ -0,0 +1,12 @@
+package desafios;
+
+//Declare uma variável do tipo char (letra) e uma variável do tipo String (palavra). Atribua valores a essas variáveis e concatene-as em uma mensagem.
+
+public class Palavras {
+    public static void main(String[] args) {
+        char letra = 'A';
+        String palavra = "marelo";
+        String mensagem = "A letra é " + letra + " e a palavra é " + palavra;
+        System.out.println(mensagem);
+    }
+}
