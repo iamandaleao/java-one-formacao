05511ed9d5b109701e9ede1c2d367bbbcea4b619
iamandaleao <amandabibiano144@gmail.com>
2025-10-13 21:36:05 -0300

    fix: update condition logic in Condicional

---
+++ src/desafios/Condicional.java
@@ -0,0 +1,20 @@
+package desafios;
+
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+        if (anoDeLancamento >= 2022) {
+            System.out.println("Lançamento que os clientes estão curtindo!");
+
+            if (incluidoNoPlano && tipoPlano.equals("plus")) {
+                System.out.println("Filme liberado");
+            } else {
+                System.out.println("Deve pagar a locação");
+            }
+        }
+    }
+}
