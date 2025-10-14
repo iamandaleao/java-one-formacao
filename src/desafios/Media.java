9a5e4050787c920ecf7018bc79c1acdc1b30e9a6
iamandaleao <amandabibiano144@gmail.com>
2025-10-13 21:38:01 -0300

    feat: calculate average in Media

---
+++ src/desafios/Media.java
@@ -0,0 +1,19 @@
+package desafios;
+
+// Crie um programa que realize a média de duas notas decimais e exiba o resultado.
+
+public class Media {
+    public static void main(String[] args) {
+        double nota1 = 8.9;
+        double nota2 = 7.5;
+        double media = (nota1 = nota2) / 2;
+
+        System.out.println("Média: " + media);
+    }
+}
+
+
+
+
+
+
