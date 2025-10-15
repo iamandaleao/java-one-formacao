62fcdb26da50a9c3b0efd55410ab05da40860b27
iamandaleao <amandabibiano144@gmail.com>
2025-10-15 17:24:17 -0300

    feat: add exerc example

---
+++ src/desafios/Comparacao.java
@@ -0,0 +1,25 @@
+package desafios;
+
+    import java.util.Scanner;
+//Peça ao usuário para inserir dois números inteiros. Compare os números e imprima uma mensagem indicando se são iguais, diferentes, o primeiro é maior ou o segundo é maior.
+
+    public class Comparacao{
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Digite o primeiro número: ");
+            int numero1 = scanner.nextInt();
+
+            System.out.print("Digite o segundo número: ");
+            int numero2 = scanner.nextInt();
+
+            if (numero1 == numero2) {
+                System.out.println("Os números são iguais");
+            } else if (numero1 > numero2) {
+                System.out.println("O primeiro número é maior");
+            } else {
+                System.out.println("O segundo número é maior");
+            }
+        }
+    }
+
---
+++ src/desafios/Fatorial.java
@@ -0,0 +1,21 @@
+package desafios;
+import java.util.Scanner;
+
+// Crie um programa que solicite ao usuário um número e calcule o fatorial desse número.
+
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        int fatorial = 1;
+
+        for (int i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+
+        System.out.println("O fatorial de " + numero + " é: " + fatorial);
+    }
+}
