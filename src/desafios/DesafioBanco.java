0c126c6a80ddaadf937afe7e5de93364047b33e3
iamandaleao <amandabibiano144@gmail.com>
2025-10-17 10:14:09 -0300

    feat: rename file

--- src/desafios/DesafioBanco.java
+++ src/desafios/DesafioBanco.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class DesafioBanco {
     public static void main(String[] args) {
-        String nome = "Clark Kent";
+        String nome = "Amanda Leão";
         String tipoConta = "Corrente";
         double saldo = 1599.99;
         int opcao = 0;
